55e59ec5e5ad617e4e3557016c1f38047955d909
vWernay <victorlwernay@gmail.com>
2025-05-23 14:29:51 -0300

    chore: convert project images from `png` to `webp` format for improved loading performance

--- src/constants/general.tsx
+++ src/constants/general.tsx
@@ -138,14 +138,14 @@ export const careerProjects: { [key: string]: PersonalProject[] } = {
   Works: [
     {
       name: "ZentriX Shop",
-      image: { url: "/images/zentrix_shop_preview.png", alt: "ZentriX Shop Website" },
+      image: { url: "/images/zentrix_shop_preview.webp", alt: "ZentriX Shop Website" },
       summary: "ZentriX Shop was started by me on 2019 as a Cfx.re (FiveM) Server, nowadays became a Shop with about 300 public customers, providing ready packages and private orders. Recently, I developed the storefront using Next 15, React 19 and Tebex API.",
       link: "https://zentrix-shop.com/",
       techs: ["Lua", "Typescript", "NodeJs", "ReactJs", "VueJs", "SCSS", "Redux", "Zustand", "MySQL", "NextJs"],
     },
     {
       name: "IF Developments",
-      image: { url: "/images/if_developments_preview.png", alt: "IF Developments Page" },
+      image: { url: "/images/if_developments_preview.webp", alt: "IF Developments Page" },
       summary: "I'm a Senior Fullstack Developer at IF Developments, which provides to the Cfx.re community the highest quality experience, using modern UI's and backend.",
       link: "https://if-developments.tebex.io/",
       techs: ["Lua", "Typescript", "ReactJs", "SCSS", "Redux", "Zustand", "MySQL"],
@@ -154,28 +154,28 @@ export const careerProjects: { [key: string]: PersonalProject[] } = {
   Freelance: [
     {
       name: "Game Dashboard",
-      image: { url: "/images/zentrix_dashboard.png", alt: 'Dashboard' },
+      image: { url: "/images/zentrix_dashboard.webp", alt: 'Dashboard' },
       summary:
         "A Game Dashboard and built-in store for a MTA server, built in 2021.",
       techs: ["Javascript", "React", "Axios", "SCSS", "MUI v4", "NodeJs", "Express", "MySQL", "Sequelize", "Payment Gateways"],
     },
     {
       name: "FuelTech NUI",
-      image: { url: "/images/brp_fueltech.png", alt: 'FuelTech In game (GTA 5)' },
+      image: { url: "/images/brp_fueltech.webp", alt: 'FuelTech In game (GTA 5)' },
       summary:
         "Developed a custom NUI interface for FiveM, simulating the functionality of the FuelTech FT450.",
       techs: ["Javascript", "jQuery", "SCSS", "MySQL", "Lua"],
     },
     {
       name: "WheelSpin NUI",
-      image: { url: "/images/brp_wheelspin.png", alt: 'WheelSpin In game (GTA 5)' },
+      image: { url: "/images/brp_wheelspin.webp", alt: 'WheelSpin In game (GTA 5)' },
       summary:
         "Developed a dynamic wheelspin script inspired by Forza Horizon 5 for FiveM. This project features dynamic prize management and a faithful recreation of the Forza wheelspin interface, delivering an authentic experience for players.",
       techs: ["TypeScript", "React", "ChakraUI", "Zustand", "Vite", "MySQL", "Lua"],
     },
     {
       name: "Groupmanager",
-      image: { url: "/images/groupmanager.png", alt: 'Groupmanager In game (GTA 5)' },
+      image: { url: "/images/groupmanager.webp", alt: 'Groupmanager In game (GTA 5)' },
       summary: "A powerful and versatile FiveM script developed over one year to provide seamless group/org management for servers. Featuring more than 10 pages, it delivers extensive functionality while supporting multi-language environments and themes. Designed with flexibility in mind, the script is entirely configurable, making it easy to adapt to various server needs and ensuring an optimal user experience.",
       techs: ["TypeScript", "React", "MUI v5", "Redux", "Vite", "MySQL", "Lua"],
     },
@@ -183,28 +183,28 @@ export const careerProjects: { [key: string]: PersonalProject[] } = {
   "Open Source": [
     {
       name: "Arduino Car Controller",
-      image: { url: "/images/gg_arduino_car_controller.png", alt: 'Arduino Controller APP' },
+      image: { url: "/images/gg_arduino_car_controller.webp", alt: 'Arduino Controller APP' },
       summary: 'It\'s a mobile application developed for the "CEM 01 do Guará" educational institution, designed to control Arduino-based BT cars made on robotics classes, using Classic Bluetooth.',
       link: "https://github.com/vWernay/gg-arduino-car-controller",
       techs: ["React Native", "Typescript", "Expo", "TailwindCSS", "Classic Bluetooth Lib"],
     },
     {
       name: "Node.js - PostgreSQL to CSV",
-      image: { url: "/images/node-pg-csv.png", alt: 'Illustration with PostgreSQL, Node.js and CSV icons.' },
+      image: { url: "/images/node-pg-csv.webp", alt: 'Illustration with PostgreSQL, Node.js and CSV icons.' },
       summary: 'A Node.js script that exports huge data from a PostgreSQL database into a CSV file using streams, pipeline and query cursor.',
       link: "https://github.com/vWernay/node-pg-csv",
       techs: ["Typescript", "NodeJs", "PostgreSQL"],
     },
     {
       name: "Plann.er (NLW Journey)",
-      image: { url: "/images/planner_preview.png", alt: 'Plann.er Page' },
+      image: { url: "/images/planner_preview.webp", alt: 'Plann.er Page' },
       summary: 'A Website designed to help you and your friends plan trips, record activities, and save useful links. The project was developed during the NLW Journey by Rocketseat in July 2024.',
       link: "https://github.com/vWernay/nlw-journey",
       techs: ["Typescript", "React", "Vite", "TailwindCSS", "Axios", "NodeJs", "Prisma", "Fastify", "Zod"],
     },
     {
       name: "Time Capsule (NLW Spacetime)",
-      image: { url: "/images/time_capsule_preview.png", alt: 'Time Capsule Previews' },
+      image: { url: "/images/time_capsule_preview.webp", alt: 'Time Capsule Previews' },
       summary: 'A full-stack application, from back-end to mobile, works like a time capsule that stores past memories in a timeline. The project was developed during the NLW Spacetime by Rocketseat in May 2023.',
       link: "https://github.com/vWernay/nlw-12-spacetime-ignite",
       techs: ["Typescript", "React", "React Native", "NextJs", "Vite", "Expo", "TailwindCSS", "Axios", "NodeJs", "Prisma", "Fastify", "Zod"],
