d3691add2487c3e03c7abc45aeb65c575cf9e33e
vWernay <victorlwernay@gmail.com>
2025-09-13 13:06:38 -0300

    refactor: update ChakraUI built-in components

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,31 +1,14 @@
 "use client"
 
-import { ChakraProvider, createSystem, defaultConfig, defaultSystem } from "@chakra-ui/react"
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
 import {
   ColorModeProvider,
   type ColorModeProviderProps,
 } from "./color-mode"
 
-const system = createSystem(defaultConfig, {
-  globalCss: {
-    "html": {
-      scrollBehavior: 'smooth',
-      scrollPaddingTop: '70px'
-    }
-  },
-  theme: {
-    tokens: {
-      fonts: {
-        heading: { value: "var(--font-space-grotesk)" },
-        body: { value: "var(--font-inter)" },
-      },
-    },
-  },
-})
-
 export function Provider(props: ColorModeProviderProps) {
   return (
-    <ChakraProvider value={system}>
+    <ChakraProvider value={defaultSystem}>
       <ColorModeProvider {...props} />
     </ChakraProvider>
   )
